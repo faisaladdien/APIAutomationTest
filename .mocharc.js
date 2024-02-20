0c3585b183391eb9508f5ed53d6e395fb6a81f31
faisaladdien <faisaladdien28@gmail.com>
2024-02-20 18:45:27 +0700

    Add reporting method for the test case using Mochawesome Report

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,13 @@
 module.exports ={
     spec : "testExplorer/testSuite1/*.js",
     timeout : 20000,
-    reporter : "spec"
+    reporter : "mochawesome", // Use mochawesome as the test report generator
+               "reporter-option": [
+                    "reportDir=report", // Report directory
+                    "reportFilename=[status]_[datetime]-[name]-report", //Report file name
+                    "html=true", // enable html report
+                    "json=false", // disable json report
+                    "overwrite=false", // disable report file overwrite
+                    "timestamp=longDate", // add timestamp to report file name
+               ], // mochawesome report generator options
 }
