d3d387083a9f7924d4d2af51cede9950b6507d7b
faisaladdien <faisaladdien28@gmail.com>
2024-02-20 14:07:30 +0700

    Add Variable and API Object

---
+++ testExplorer/apiServer/apiObject.js
@@ -0,0 +1,6 @@
+const request = require("supertest")
+const {expect} = require("chai")
+const url = require("../variable/globalVariable")
+
+const baseURL = `${url}`
+
---
+++ testExplorer/variable/globalVariable.js
@@ -0,0 +1,3 @@
+const url = "https://api.restful-api.dev/"
+
+module.exports = url
